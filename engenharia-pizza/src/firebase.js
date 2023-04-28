fab5b8db683582285fb57b5972f5756c9ccc3594
Artur Alves <arturacb@gmail.com>
2023-04-27 21:06:55 -0300

    add app build

---
+++ engenharia-pizza/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4xW6nPAPQnQBqgZFI7NlAuC5XRh09cAc",
+  authDomain: "engenharia-da-pizza-f4be5.firebaseapp.com",
+  projectId: "engenharia-da-pizza-f4be5",
+  storageBucket: "engenharia-da-pizza-f4be5.appspot.com",
+  messagingSenderId: "512466033746",
+  appId: "1:512466033746:web:70922f09ad48f1e230d38b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
