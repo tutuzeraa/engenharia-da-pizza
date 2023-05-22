ca1995fffe7cdb6f15984f1bb887cb26c4eee9ec
tutuzera <arturacb@gmail.com>
2023-05-21 22:45:54 -0300

    add requirements.md

---
+++ REQUIREMENTS.md
@@ -0,0 +1,13 @@
+Utilizamos o processo de Design Thinking para identificar e eleger os requisitos necessários. O Design Thinking é uma abordagem que visa resolver problemas de forma criativa e centrada no usuário, permitindo uma compreensão mais profunda das necessidades e desejos das pessoas envolvidas.
+A primeira etapa que realizamos foi a definição de perguntas-chave relacionadas ao problema que estávamos abordando. Essas perguntas foram formuladas com base em nosso entendimento inicial da situação e nos objetivos do projeto. Elas serviram como guia para direcionar nossa pesquisa e análise posterior. São elas: “Quem é o usuário Final” e “O que é o problema do usuário final?”.
+Em seguida, entrevistamos uma voluntária do Operação Natal e várias pessoas ligadas à organização para obter insights valiosos. A entrevista com a usuária está na mesma pasta deste arquivo no repositório. Durante as entrevistas, buscamos compreender suas experiências, desafios enfrentados e pontos de vista sobre os processos existentes. Essa interação direta nos permitiu coletar informações qualitativas e compreender melhor as necessidades e expectativas dos usuários.
+Após as entrevistas, realizamos uma análise dos dados coletados. Identificamos os principais pontos de dor e gargalos nos processos atuais da organização. Isso nos ajudou a compreender as lacunas e áreas de melhoria, além de fornecer insights valiosos para o desenvolvimento das soluções.
+Com base nos resultados da análise, respondemos às perguntas-chave que havíamos estabelecido anteriormente. Os usuários finais são organizadores de evento e voluntários da Operação Natal, e seu maior problema é a dificuldade de organizar turnos de trabalho nos dias de arrecadação de alimentos.
+Por fim, utilizando todas as informações coletadas, criamos uma árvore de oportunidades. Essa árvore nos ajudou a visualizar e explorar diferentes caminhos e soluções possíveis. Ela nos permitiu avaliar a viabilidade e o impacto de cada uma delas. Através desse processo, pudemos eleger as soluções mais promissoras e relevantes para atender aos requisitos do projeto.
+Em resumo, ao utilizar o processo de Design Thinking, o time passou pelas etapas de elencar perguntas-chave, realizar entrevistas, identificar pontos de dor, responder às perguntas-chave e criar uma árvore de oportunidades.
+Após as etapas anteriores e com base no brainstorming e novas discussões realizadas pelo time,  elegemos os seguintes requisitos para o projeto:
+gerenciar os usuários da plataforma
+facilitar o controle de eventos pelos organizadores
+facilitar o processo de voluntariado
+
+O levantamento de Stories a partir desses épicos pode ser visto no arquivo "Levantamento de Requisitos - Figma" nesta pasta.
--- engenharia-da-pizza/src/pages/form/tests/disponibilidade.test.js
+++ engenharia-da-pizza/src/pages/form/tests/disponibilidade.test.js
@@ -1,91 +1,128 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
-import "@testing-library/jest-dom";
+// import "@testing-library/jest-dom";
 import AvailabilityForm from '../AvailabilityForm';
 
-jest.mock('firebase/compat/app', () => {
-  return {
-    firestore: jest.fn(() => {
-      return {
-        collection: jest.fn(() => {
-          return {
-            doc: jest.fn(() => {
-              return {
-                update: jest.fn(() => {
-                  return Promise.resolve();
-                })
-              };
-            })
-          };
-        })
-      };
-    })
-  };
+describe('AvailabilityForm', () => {
+  test('renders the form correctly', () => {
+    render(<AvailabilityForm />);
+    expect(screen.getByText('Marque sua Disponibilidade')).toBeInTheDocument();
+  });
+
+  test('allows the user to add availability', () => {
+    render(<AvailabilityForm />);
+    fireEvent.change(screen.getByLabelText('Clique para adicionar data e horário'), { target: { value: '05/09/2023 09:00' } });
+    fireEvent.click(screen.getByText('Adicionar'));
+    expect(screen.getByText('05/09/2023 09:00')).toBeInTheDocument();
+  });
+
+  test('allows the user to edit availability', () => {
+    render(<AvailabilityForm />);
+    fireEvent.change(screen.getByLabelText('Clique para adicionar data e horário'), { target: { value: '05/09/2023 09:00' } });
+    fireEvent.click(screen.getByText('Adicionar'));
+    expect(screen.getByText('05/09/2023 09:00')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('Editar'));
+    expect(screen.getByDisplayValue('05/09/2023 09:00')).toBeInTheDocument();
+    fireEvent.change(screen.getByLabelText('Clique para adicionar data e horário'), { target: { value: '05/09/2023 10:00' } });
+    fireEvent.click(screen.getByText('Atualizar'));
+    expect(screen.getByText('05/09/2023 10:00')).toBeInTheDocument();
+  });
+
+  test('allows the user to delete availability', () => {
+    render(<AvailabilityForm />);
+    fireEvent.change(screen.getByLabelText('Clique para adicionar data e horário'), { target: { value: '05/09/2023 09:00' } });
+    fireEvent.click(screen.getByText('Adicionar'));
+    expect(screen.getByText('05/09/2023 09:00')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('Apagar'));
+    expect(screen.queryByText('05/09/2023 09:00')).not.toBeInTheDocument();
+  });
+
+  // Add more tests as needed
 });
 
-describe("RendersAvailability", () => {
+// jest.mock('firebase/compat/app', () => {
+//   return {
+//     firestore: jest.fn(() => {
+//       return {
+//         collection: jest.fn(() => {
+//           return {
+//             doc: jest.fn(() => {
+//               return {
+//                 update: jest.fn(() => {
+//                   return Promise.resolve();
+//                 })
+//               };
+//             })
+//           };
+//         })
+//       };
+//     })
+//   };
+// });
 
-  test('renders "Enter your availability" text', () => {
-    render(<AvailabilityForm />);
-    const availabilityText = screen.getByText(/Enter your availability/i);
-    expect(availabilityText).toBeInTheDocument();
-  });
+// describe("RendersAvailability", () => {
 
-  test('renders "Select your availability:" text', () => {
-    render(<AvailabilityForm />);
-    const selectAvailabilityText = screen.getByText(/Select your availability:/i);
-    expect(selectAvailabilityText).toBeInTheDocument();
-  });
+//   test('renders "Enter your availability" text', () => {
+//     render(<AvailabilityForm />);
+//     const availabilityText = screen.getByText(/Enter your availability/i);
+//     expect(availabilityText).toBeInTheDocument();
+//   });
 
-  test('renders "Add" button', () => {
-    render(<AvailabilityForm />);
-    const addButton = screen.getByText(/Add/i);
-    expect(addButton).toBeInTheDocument();
-  });
+//   test('renders "Select your availability:" text', () => {
+//     render(<AvailabilityForm />);
+//     const selectAvailabilityText = screen.getByText(/Select your availability:/i);
+//     expect(selectAvailabilityText).toBeInTheDocument();
+//   });
 
-  test('renders "Clear All" button when there are availabilities', () => {
-    render(<AvailabilityForm />);
-    const addButton = screen.getByText(/Add/i);
-    fireEvent.click(addButton);
-    const clearAllButton = screen.getByText(/Clear All/i);
-    expect(clearAllButton).toBeInTheDocument();
-  });
+//   test('renders "Add" button', () => {
+//     render(<AvailabilityForm />);
+//     const addButton = screen.getByText(/Add/i);
+//     expect(addButton).toBeInTheDocument();
+//   });
 
-  test('renders "Save" button when there are availabilities', () => {
-    render(<AvailabilityForm />);
-    const addButton = screen.getByText(/Add/i);
-    fireEvent.click(addButton);
-    const saveButton = screen.getByText(/Save/i);
-    expect(saveButton).toBeInTheDocument();
-  });
+//   test('renders "Clear All" button when there are availabilities', () => {
+//     render(<AvailabilityForm />);
+//     const addButton = screen.getByText(/Add/i);
+//     fireEvent.click(addButton);
+//     const clearAllButton = screen.getByText(/Clear All/i);
+//     expect(clearAllButton).toBeInTheDocument();
+//   });
 
-  test('renders "Edit" button for each availability', () => {
-    render(<AvailabilityForm />);
-    const addButton = screen.getByText(/Add/i);
-    fireEvent.click(addButton);
-    const editButton = screen.getByText(/Edit/i);
-    expect(editButton).toBeInTheDocument();
-  });
-})
+//   test('renders "Save" button when there are availabilities', () => {
+//     render(<AvailabilityForm />);
+//     const addButton = screen.getByText(/Add/i);
+//     fireEvent.click(addButton);
+//     const saveButton = screen.getByText(/Save/i);
+//     expect(saveButton).toBeInTheDocument();
+//   });
+
+//   test('renders "Edit" button for each availability', () => {
+//     render(<AvailabilityForm />);
+//     const addButton = screen.getByText(/Add/i);
+//     fireEvent.click(addButton);
+//     const editButton = screen.getByText(/Edit/i);
+//     expect(editButton).toBeInTheDocument();
+//   });
+// })
 
 
-describe("ButtonsWorking", () => {
-  test('submitting form with valid date and time adds availability', () => {
-    const { getByText, getByLabelText } = render(<AvailabilityForm />);
-    const dateTimePickerElement = getByText(/Date and time/i);
-    const addButtonElement = getByText(/Add/i);
-    const expectedDateTime = '2023-05-01T10:00:00.000Z';
+// describe("ButtonsWorking", () => {
+//   test('submitting form with valid date and time adds availability', () => {
+//     const { getByText, getByLabelText } = render(<AvailabilityForm />);
+//     const dateTimePickerElement = getByText(/Date and time/i);
+//     const addButtonElement = getByText(/Add/i);
+//     const expectedDateTime = '2023-05-01T10:00:00.000Z';
   
-    // Set date and time
-    fireEvent.change(dateTimePickerElement, { target: { value: expectedDateTime } });
+//     // Set date and time
+//     fireEvent.change(dateTimePickerElement, { target: { value: expectedDateTime } });
   
-    // Submit form
-    fireEvent.click(addButtonElement);
+//     // Submit form
+//     fireEvent.click(addButtonElement);
   
-    // Verify that availability was added to the list
-    const availabilityElement = getByText(/10:00 AM/i);
-    expect(availabilityElement).toBeInTheDocument();
-  });
+//     // Verify that availability was added to the list
+//     const availabilityElement = getByText(/10:00 AM/i);
+//     expect(availabilityElement).toBeInTheDocument();
+//   });
 //   test('clicking "Clear All" button removes all availabilities', () => {
 //     render(<AvailabilityForm />);
 //     const addButton = screen.getByText(/Add/i);
@@ -118,4 +155,4 @@ describe("ButtonsWorking", () => {
 //     expect(firestoreUpdateMock).toHaveBeenCalled();
 //   });
 
-})
+// })
