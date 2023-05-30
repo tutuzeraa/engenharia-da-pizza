39d691c8747adc6efc0809e7b141d51bfa60102d
pedro.bordignon <pedro.bordignon@conpec.com.br>
2023-05-30 16:05:41 -0300

    padrao de projeto aplicado registro e home

--- agendando_um_jeito/src/contexts/authContext.jsx
+++ agendando_um_jeito/src/contexts/authContext.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
-import { auth } from '../services/firebase';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../services/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 export const AuthContext = React.createContext({});
 
 export const AuthProvider = ({ children }) => {
     const [signed, setSigned] = useState(false)
     const [user, setUser] = useState(null);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -17,8 +21,6 @@ export const AuthProvider = ({ children }) => {
                 setUser(null);
             }
         })
-
-        // * cleanup;
         return unsubscribe;
     }, [])
 
@@ -27,7 +29,24 @@ export const AuthProvider = ({ children }) => {
         return signOut(auth)
     }
 
-    const value = { signed, setSigned, user, setUser, logout };
+    const Signup = async (e) => {
+        e.preventDefault();
+
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+            const user = userCredential.user;
+
+            const usersCollectionRef = doc(db, 'users', user.uid);
+            await setDoc(usersCollectionRef, { email, password })
+
+            setEmail("");
+            setPassword("");
+        } catch (error) {
+            console.log('error: ', error);
+        }
+    }
+
+    const value = { signed, setSigned, user, setUser, logout, Signup, email, setEmail, password, setPassword };
 
     return (
         <AuthContext.Provider value={value}>
---
+++ agendando_um_jeito/src/pages/register/viewRegister.jsx
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import logo from "../../assets/logo.svg"
+import "./styles.css"
+
+
+
+export function ViewRegister({ Signup, setEmail, setPassword }) {
+    return (
+        <div className="container">
+            <header className="header">
+                <img src={logo} alt="Workflow logo" />
+                <span>Informe seus dados de cadastro</span>
+            </header>
+
+            <form>
+                <div className="inputContainer">
+                    <label htmlFor="email">E-Mail</label>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder="johndoe@gmail.com"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div className="inputContainer">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="***************"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+
+                <button onClick={Signup} className="button" >
+                    Cadastrar
+                    <img src="" alt="" />
+                </button>
+
+                <div className="footer">
+                    <p>Já tem conta? </p>
+                    <Link to="../">Faça login :)</Link>
+
+                </div>
+
+            </form>
+        </div>
+    );
+}
+
