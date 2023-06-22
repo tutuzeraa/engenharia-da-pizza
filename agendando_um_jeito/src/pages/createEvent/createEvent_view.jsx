2728396572761b9a44ad5eba233b6e0ea0d3e1f8
João Deltregia Martinelli <j248342@dac.unicamp.br>
2023-06-21 22:44:13 -0300

    modifiquei availability form pra ter data inicial e final e embelezei criar turno

--- agendando_um_jeito/src/pages/createEvent/createEvent_view.jsx
+++ agendando_um_jeito/src/pages/createEvent/createEvent_view.jsx
@@ -1,88 +1,89 @@
 import { BrowserRouter, Link, Router } from "react-router-dom";
-import logo from "../../assets/logo.svg"
-//import "./styles.css"
-import React from 'react'
+import logo from "../../assets/logo.svg";
+import React from 'react';
+import './styles.css';
 
-
-export function ViewCreateEvent({SignIn, SignInWithGoogle, setEmail, setPassword}) {
+export function ViewCreateEvent({ SignIn, SignInWithGoogle, setEmail, setPassword }) {
   return (
     <div className="modal">
-    <h1 className="tituloModal">Criar Turno</h1> 
-    <form>
-    <div className="inputContainer">
-        <label>Nome do Evento</label>
-        <input type="text"
-        name="nomeEvento"
-        id="nomeEvento"
-        placeholder="Insira um nome para seu evento"/>
-    </div>
+      <h1 className="tituloModal">Criar Turno</h1>
+      <form>
+        <div className="inputContainer">
+          <label>Nome do Evento</label>
+          <input
+            type="text"
+            name="nomeEvento"
+            id="nomeEvento"
+            placeholder="Insira um nome para seu evento" />
+        </div>
 
-    <div className="inputContainer">
-        <label>Nome do responsável</label>
-        <input type="text"
-        name="nomeResponsavel"
-        id="nomeResponsavel"
-        placeholder="Insira o nome da pessoa responsável"/>
-    </div>
+        <div className="inputContainer">
+          <label>Nome do responsável</label>
+          <input
+            type="text"
+            name="nomeResponsavel"
+            id="nomeResponsavel"
+            placeholder="Insira o nome da pessoa responsável" />
+        </div>
 
-    <div className="inputContainer">
-        <label>Telefone do responsável</label>
-        <input type="text"
-        name="telefone"
-        id="telefone"
-        placeholder="Insira o telefone da pessoa responsável"/>
-    </div>
+        <div className="inputContainer">
+          <label>Telefone do responsável</label>
+          <input
+            type="text"
+            name="telefone"
+            id="telefone"
+            placeholder="Insira o telefone da pessoa responsável" />
+        </div>
 
-    <div className="inputContainer">
-        <label>Selecione o dia da Semana</label>
-        <br/>
-        <input 
+        <div className="inputContainer">
+          <label>Selecione o dia da Semana</label>
+          <br />
+          <input
             type="checkbox"
             id="segunda"
-            name="segunda" 
-            value="segunda"/>
-        <label>segunda</label>
+            name="segunda"
+            value="segunda" />
+          <label>segunda</label>
 
-        <br/>
-        <input 
+          <br />
+          <input
             type="checkbox"
             id="terca"
-            name="terca" 
-            value="terca"/>
-        <label>terca</label>
+            name="terca"
+            value="terca" />
+          <label>terca</label>
 
-        <br/>
-        <input 
+          <br />
+          <input
             type="checkbox"
             id="quarta"
-            name="quarta" 
-            value="quarta"/>
-        <label>quarta</label>
+            name="quarta"
+            value="quarta" />
+          <label>quarta</label>
 
-        <br/>
-        <input 
+          <br />
+          <input
             type="checkbox"
             id="quinta"
-            name="quinta" 
-            value="quinta"/>
-        <label>quinta</label>
+            name="quinta"
+            value="quinta" />
+          <label>quinta</label>
 
-        <br/>
-        <input 
+          <br />
+          <input
             type="checkbox"
             id="sexta"
-            name="sexta" 
-            value="sexta"/>
-        <label>sexta</label>  
-    </div>
+            name="sexta"
+            value="sexta" />
+          <label>sexta</label>
+        </div>
 
-    <button className="button">
-            Continuar
-            <img src="" alt="" />
-    </button>
+        <button className="button">
+          Continuar
+          <img src="" alt="" />
+        </button>
 
-    </form>
+      </form>
     </div>
   );
-
 }
