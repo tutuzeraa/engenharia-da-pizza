2919dd70229e4ed3d9c23db753880853ad485510
Fernanda Sanches F. Silva <fe.sanches.sfs@gmail.com>
2023-06-30 20:13:48 +0000

    Algumas alteracoes no teste do email feitas junto aPedro Bordignom

--- agendando_um_jeito/src/tests/Email.test.js
+++ agendando_um_jeito/src/tests/Email.test.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import index from '../pages/login/index';
+import { render } from '@testing-library/react';
+import LoginForm from '../pages/login/index';
 
-function isValidEmail(index) {
-    const { getByLabelText, getByText } = render(<LoginForm />);
-    const emailInput = getByLabelText('email');
-    return /\S+@\S+\.\S+/.test(emailInput);
-  }
+function isValidEmail(email) {
+  const { getByLabelText } = render(<LoginForm />);
+  const emailInput = getByLabelText('email');
+  return /\S+@\S+\.\S+/.test(emailInput.value);
+}
 
-describe('isValidEmail', () => {
-    it('should return true for valid email format', () => {
-      const validEmails = [
-        'user@example.com',
-        'john.doe@gmail.com',
-        'test123@hotmail.com',
-      ];
-  
-      validEmails.forEach((index) => {
-        expect(isValidEmail(index)).toBe(true);
-      });
-    });
-  
-    it('should return false for invalid email format', () => {
-      const invalidEmails = [
-        'userexample.com',
-        'john.doe@gmailcom',
-        'test123@.com',
-        'invalid-email',
-      ];
-  
-      invalidEmails.forEach((index) => {
-        expect(isValidEmail(index)).toBe(false);
-      });
+describe('isValidEmailTest', () => {
+  it('should return true for valid email format', () => {
+    const validEmails = [
+      'user@example.com',
+      'john.doe@gmail.com',
+      'test123@hotmail.com',
+    ];
+
+    validEmails.forEach((email) => {
+      expect(isValidEmail(email)).toBe(true);
     });
   });
-  
+
+  it('should return false for invalid email format', () => {
+    const invalidEmails = [
+      'userexample.com',
+      'john.doe@gmailcom',
+      'test123@.com',
+      'invalid-email',
+    ];
+
+    invalidEmails.forEach((email) => {
+      expect(isValidEmail(email)).toBe(false);
+    });
+  });
+});
